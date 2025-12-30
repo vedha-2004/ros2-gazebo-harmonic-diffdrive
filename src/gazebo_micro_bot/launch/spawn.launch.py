b1a2f2f3b74e275488c5085360e0590830922942
Vedha-2004 <yalluruvedhareddy@gmail.com>
2025-12-31 01:05:26 +0530

    Fix TF bridging and QoS for ROS 2 Jazzy + Gazebo Harmonic

--- src/gazebo_micro_bot/launch/spawn.launch.py
+++ src/gazebo_micro_bot/launch/spawn.launch.py
@@ -4,6 +4,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
 
     pkg_path = get_package_share_directory('gazebo_micro_bot')
@@ -11,11 +12,13 @@ def generate_launch_description():
 
     return LaunchDescription([
 
+        # 1. Start Gazebo Harmonic
         ExecuteProcess(
             cmd=['gz', 'sim', '-r', 'empty.sdf'],
             output='screen'
         ),
 
+        # 2. Spawn the robot
         Node(
             package='ros_gz_sim',
             executable='create',
@@ -25,5 +28,44 @@ def generate_launch_description():
                 '-z', '0.1'
             ],
             output='screen'
+        ),
+
+        # 3. Bridge cmd_vel (ROS -> Gazebo)
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
+            ],
+            output='screen'
+        ),
+
+        # 4. Bridge TF (Gazebo -> ROS /tf)
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                '/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V'
+            ],
+            parameters=[{
+                'qos_overrides./tf.publisher.durability': 'volatile',
+                'qos_overrides./tf.publisher.reliability': 'reliable'
+            }],
+            output='screen'
+        ),
+
+        # 5. Bridge static TF (Gazebo -> ROS /tf_static)
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=[
+                '/tf_static@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V'
+            ],
+            parameters=[{
+                'qos_overrides./tf_static.publisher.durability': 'transient_local',
+                'qos_overrides./tf_static.publisher.reliability': 'reliable'
+            }],
+            output='screen'
         )
+
     ])
